5e95a5dee02b12d5999dea9e635eba83b784dd44
nxf37731 <nxf37731@GBBWI153.wbi.nxp.com>
2017-11-27 12:42:51 +0100

    first commit

---
+++ Dummy/src/com/Dummy.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * @author nxf37731
+ *
+ */
+public class Dummy {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hai");
+
+	}
+
+}
